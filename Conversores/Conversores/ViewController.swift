11afb1f262ca75033e799c50454341e66f364143
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-07-13 12:04:38 -0300

    Create convert values

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Vinicius on 13/07/2018.
+//  Copyright © 2018 Vinicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var value: UITextField!
+
+    @IBOutlet weak var unitF: UIButton!
+    @IBOutlet weak var unitC: UIButton!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var resultUnit: UILabel!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBAction func showNext(_ sender: Any) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
