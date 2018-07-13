365019cfc5b4ca0e76e5ecf94fef1f0c826796e6
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-07-13 01:00:14 -0300

    IMC controller implemented

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -24,24 +24,54 @@ class ViewController: UIViewController {
 
 
     @IBAction func calculoImc(_ sender: Any) {
-        
         if(alturaField == nil && pesoField == nil) {
             return
         }
-        var imc : Double
-         let altura = Double(alturaField.text!)
-         let peso = Double(pesoField.text!)
-        
-        imc = peso! / (altura! * altura!)
-
-        result.text = String(format:"%f", imc)
-        
-        
-        
+        calculeImc()
         
         
     }
     
+    func calculeImc() {
+        let altura = Double(alturaField.text!)
+        let peso = Double(pesoField.text!)
+        var imc : Double
+       
+        if(alturaField.text == "" && pesoField.text == "") {
+            return
+        }
+    
+        imc = peso! / (altura! * altura!)
+        
+        var imcConverted = String(format:"%.1f", imc)
+    
+        switch imc{
+        case 19...25:
+            imageResult.image = #imageLiteral(resourceName: "ideal")
+            result.text = imcConverted
+            
+        case 0...16:
+            imageResult.image = #imageLiteral(resourceName: "magreza")
+            result.text = imcConverted
+            
+        case 25...30:
+            imageResult.image = #imageLiteral(resourceName: "sobre")
+            result.text = imcConverted
+            
+        case 30...100:
+            imageResult.image = #imageLiteral(resourceName: "obesidade")
+            result.text = imcConverted
+            
+        case 16...18.9:
+            imageResult.image = #imageLiteral(resourceName: "abaixo")
+            result.text = imcConverted
+            
+        default:
+            print("Default")
+        }
+        
+        
+    }
     
     
 
