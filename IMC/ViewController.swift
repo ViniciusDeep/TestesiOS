c1175045461f65fc213b4fc1f3fea16d30c3aeff
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-07-12 22:58:33 -0300

    Apply auto layuot

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    
+    @IBOutlet weak var alturaField: UITextField!
+    @IBOutlet weak var pesoField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func calculoImc(_ sender: Any) {
+    }
+    
+    
+    
 
 }
 
