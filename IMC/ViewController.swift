9da5302a1bd747ea988078388e699a7f1171df04
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-07-12 23:14:57 -0300

    IMC calculated

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var alturaField: UITextField!
     @IBOutlet weak var pesoField: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var imageResult: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,22 @@ class ViewController: UIViewController {
 
 
     @IBAction func calculoImc(_ sender: Any) {
+        
+        if(alturaField == nil && pesoField == nil) {
+            return
+        }
+        var imc : Double
+         let altura = Double(alturaField.text!)
+         let peso = Double(pesoField.text!)
+        
+        imc = peso! / (altura! * altura!)
+
+        result.text = String(format:"%f", imc)
+        
+        
+        
+        
+        
     }
     
     
