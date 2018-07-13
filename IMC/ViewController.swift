65ee948ec372929bff3895b5ca3acd2f3b40eb90
ViniciusDeep <viniciuscorreia11@gmail.com>
2018-07-12 21:25:32 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Vinicius on 12/07/2018.
+//  Copyright © 2018 Vinicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
